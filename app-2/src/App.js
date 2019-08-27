61dc9c238f3c1f2ab2c86a38081616e2fdd88e75
Tate Stone <tatejamesstone@gmail.com>
2019-08-27 15:28:29 -0600

    Finished App 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+     cities: ['New York', 'Chicago', 'London', 'Berlin', 'Tokyo', 'San Francisco']
+    }
+  }
+
+
   render() {
+    const list = this.state.cities.map((val, i) => {
+      return <h2 key={i}>{val}</h2>
+    }
+    )
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+      {list}
+        
       </div>
     );
   }
