400de29c52119713075cbfcaff6239867aac60f4
Tate Stone <tatejamesstone@gmail.com>
2019-08-27 09:00:43 -0600

    Finished App 4

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -3,16 +3,43 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.handleUpdateUsername = this.handleUpdateUsername.bind(this)
+    this.handleUpdatePassword = this.handleUpdatePassword.bind(this)
+    this.login = this.login.bind(this)
+  }
+
+  handleUpdateUsername(event){
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleUpdatePassword(event){
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  login(){
+    alert(`Username: ${this.state.username} Password: ${this.state.password}`)
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={this.handleUpdateUsername} placeholder="username" className="username"/>
+        <input onChange={this.handleUpdatePassword} placeholder="password" className="password"/>
+        <button onClick={this.login} className="login">Login</button>
+
       </div>
     );
   }
