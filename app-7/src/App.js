cdf2fdcaee8b32e47c6c60f77cdb74d4470b36d0
Tate Stone <tatejamesstone@gmail.com>
2019-08-27 17:05:01 -0600

    Finished

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,40 +1,30 @@
 import React, { Component } from "react";
-
+import logo from "./logo.svg";
 import "./App.css";
-import Todo from './Todo'
-import List from './List'
-import NewTask from './NewTask'
+import NewTask from "./NewTask";
+import List from "./List";
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
+
     this.state = {
-      list: [],
-    }
-    this.handleAddTask = this.handleAddTask.bind(this)
+      list: []
+    };
+
+    this.handleAddTask = this.handleAddTask.bind(this);
   }
 
-  
-
-  handleAddTask(event){
-    this.setState({
-      list: [...this.state.list, this.state.input],
-      
-    })
+  handleAddTask(task) {
+    this.setState({ list: [...this.state.list, task] });
   }
-
-
-
 
   render() {
-    
     return (
       <div className="App">
         <h1>My to-do list:</h1>
         <NewTask add={this.handleAddTask} />
-        <List tasks={this.state.list}/>
-
-        
+        <List tasks={this.state.list} />
       </div>
     );
   }
--- app-7/src/List.jsx
+++ app-7/src/List.jsx
@@ -1,24 +1,12 @@
-import React, {Component} from 'react'
-import Todo from './Todo'
+import React, {Component} from 'react';
+import Todo from "./Todo";
 
-export default class List extends Component{
-
-    constructor(){
-        super()
-
-    }
-
-    render(){
-        let list = this.props.map((element, index) => {
-            return <Todo key={index} task={element} />;
-          });
-        
-            return (
-                <div>
-                    {list}
-                </div>
-            )
-        
-    }
+export default class List extends Component {
+  render() {
+    let list = this.props.tasks.map((element, index) => {
+      return <Todo key={index} task={element} />;
+    });
+    
+    return <div>{list}</div>;
+  }
 }
-
