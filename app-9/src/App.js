829fb8824825760c8e78ac81a920611d246faee0
Tate Stone <tatejamesstone@gmail.com>
2019-09-12 08:03:45 -0600

    Finished

--- app-9/src/App.js
+++ app-9/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
+import {Link} from 'react-router-dom'
+import router from "./Component/router";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <nav>
+          <ul>
+            <Link to='/'>Home</Link>
+            <Link to='/details'>Details</Link>
+            <Link to='/signup'>Signup</Link>
+          </ul>
+        </nav>
+
+        <br/>
+
+        {router}
       </div>
     );
   }
--- app-9/src/Component/router.js
+++ app-9/src/Component/router.js
@@ -4,3 +4,11 @@ import {Switch, Route} from 'react-router-dom'
 import Home from './Home'
 import Signup from './Signup'
 import Details from './Details'
+
+export default (
+    <Switch>
+       <Route component={Home} exact path='/'/>
+       <Route component={Details} path='/details'/>
+       <Route component={Signup} path='/signup'/>
+    </Switch>
+)
